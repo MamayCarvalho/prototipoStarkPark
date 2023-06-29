54e398f5d0e2f396c21c85a7a7eeb75ce36d9341
MarcosHogs97 <marcos.silva171@aluno.senai.br>
2023-06-28 21:18:04 -0300

    #

--- JAVASCRIPT/homePage.js
+++ JAVASCRIPT/homePage.js
@@ -2,14 +2,24 @@ let ctx = document.getElementById('chart').getContext('2d');
 
 let data = {
     labels: ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'],
-    datasets: [{
-        label: 'Quantidade de Veículos',
-        data: [100, 45, 69, 40, 58, 80, 100],
-        backgroundColor: 'rgb(196, 160, 0, 0.5)', // Cor das barras (branco com transparência)
-        borderColor: 'rgb(255, 196, 0)', // Cor das bordas das barras (branco)
-        borderWidth: 1, // Espessura das bordas das barras
-        color: 'rgba(255, 255, 255, 1)' // Cor do texto (branco)
-    }]
+    datasets: [
+        {
+            label: 'Quantidade de Carros',
+            data: [80, 41, 52, 39, 48, 73, 80],
+            backgroundColor: 'rgb(196, 160, 0, 0.5)',
+            borderColor: 'rgb(255, 195, 0)',
+            borderWidth: 1,
+            color: 'rgba(255, 255, 255, 1)'
+        },
+        {
+            label: 'Quantidade de Motos',
+            data: [20, 15, 8, 9, 13, 18, 20],
+            backgroundColor: 'rgb(195, 195, 195, 0.5)',
+            borderColor: 'rgb(255, 255, 255)',
+            borderWidth: 1,
+            color: 'rgba(255, 255, 255, 1)'
+        }
+    ]
 };
 
 let options = {
@@ -19,7 +29,7 @@ let options = {
             beginAtZero: true,
             stepSize: 10,
             grid: {
-                color: 'rgba(255, 255, 255, 0.2)' // Cor das linhas do grid (branco com transparência)
+                color: 'rgba(255, 255, 255, 0.5)' // Cor das linhas do grid (branco com transparência)
             },
             ticks: {
                 color: 'rgba(255, 255, 255, 1)' // Cor dos valores do eixo y (branco)
