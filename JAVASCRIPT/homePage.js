7886da9c37d02cd45a14d44f2423ee8584895c2e
MarcosHogs97 <marcos.silva171@aluno.senai.br>
2023-06-30 21:49:38 -0300

    #

--- JAVASCRIPT/homePage.js
+++ JAVASCRIPT/homePage.js
@@ -1,57 +1,11 @@
-let ctx = document.getElementById('chart').getContext('2d');
+let quantidadeCarros = Math.floor(Math.random() * 700) + 1;
+let quantidadeMotos = Math.floor(Math.random() * 200) + 1;
 
-let data = {
-    labels: ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'],
-    datasets: [
-        {
-            label: 'Quantidade de Carros',
-            data: [80, 41, 52, 39, 48, 73, 80],
-            backgroundColor: 'rgb(196, 160, 0, 0.5)',
-            borderColor: 'rgb(255, 195, 0)',
-            borderWidth: 1,
-            color: 'rgba(255, 255, 255, 1)'
-        },
-        {
-            label: 'Quantidade de Motos',
-            data: [20, 15, 8, 9, 13, 18, 20],
-            backgroundColor: 'rgb(195, 195, 195, 0.5)',
-            borderColor: 'rgb(255, 255, 255)',
-            borderWidth: 1,
-            color: 'rgba(255, 255, 255, 1)'
-        }
-    ]
-};
+let quantidadeCarrosDois = Math.floor(Math.random() * 23000 + 500) + 1;
+let quantidadeMotosDois = Math.floor(Math.random() * 12000 + 500) + 1;
 
-let options = {
-    responsive: true,
-    scales: {
-        y: {
-            beginAtZero: true,
-            stepSize: 10,
-            grid: {
-                color: 'rgba(255, 255, 255, 0.5)' // Cor das linhas do grid (branco com transparência)
-            },
-            ticks: {
-                color: 'rgba(255, 255, 255, 1)' // Cor dos valores do eixo y (branco)
-            }
-        },
-        x: {
-            ticks: {
-                color: 'rgba(255, 255, 255, 1)' // Cor dos valores do eixo x (branco)
-            }
-        }
-    },
-    plugins: {
-        legend: {
-            labels: {
-                color: 'rgba(255, 255, 255, 1)' // Cor das legendas (branco)
-            }
-        }
-    }
-};
+document.getElementById('carros').innerText = quantidadeCarros;
+document.getElementById('motos').innerText = quantidadeMotos;
 
-let chart = new Chart(ctx, {
-    type: 'bar',
-    data: data,
-    options: options
-});
+document.getElementById('carrosDois').innerText = quantidadeCarrosDois;
+document.getElementById('motosDois').innerText = quantidadeMotosDois;
